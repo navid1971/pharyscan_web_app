ccba699cd8639bd3a8de8d49a634008372710c8a
navid1971 <mdazrafulislamkhan@gmail.com>
2026-05-07 07:47:32 +0600

    Update app.py

--- app.py
+++ app.py
@@ -272,5 +272,5 @@ except Exception as e:
 # Local run
 # -----------------------------
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 7860))
     app.run(host="0.0.0.0", port=port)
